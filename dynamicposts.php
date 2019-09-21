9e78a5363617b86c03c5ebf605889a94ed79293b
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-22 02:40:49 +0530

    Update dynamicposts.php

--- dynamicposts.php
+++ dynamicposts.php
@@ -28,7 +28,7 @@ function catposts_shortcode( $atts ) {
     
        echo '
           <tr>
-            <td>'.get_the_date('F jS,Y,l | H:i').' '.$apm.'</td>
+            <td>'.get_the_date('F jS,Y,l | g:i').' '.$apm.'</td>
             <td><a class="indigo-text" style="font-size:16px;text-decoration:none;" href="'.get_the_permalink().'"  style="text-decoration:none;font-size:14px;color:black;font-weight:lighter;">'.get_the_title().'</a>&nbsp;</td>
             <td>Not Set</td>// custom field data can be used and it is given in the readme.md
           </tr>';
